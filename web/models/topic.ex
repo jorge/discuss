25bd28adeab24b20b045fa741771202e6e8c0ec4
Jorge Chao <jorge@chao.haus>
2018-06-25 14:19:42 -0500

    added comments and sockets.

--- web/models/topic.ex
+++ web/models/topic.ex
@@ -3,6 +3,8 @@ defmodule Discuss.Topic do
 
   schema "topics" do
     field :title, :string
+    belongs_to :user, Discuss.User
+    has_many :comments, Discuss.Comment
   end
 
   def changeset(struct, params \\ %{}) do
@@ -10,5 +12,4 @@ defmodule Discuss.Topic do
     |> cast(params, [:title])
     |> validate_required([:title])
   end
-
 end
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -53,10 +53,51 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+const createSocket = (topicId) => {
+  let channel = socket.channel(`comments:${topicId}`, {})
+  channel.join()
+    .receive("ok", resp => {
+      console.log(resp)
+      renderComments(resp.comments)
+    })
 
-export default socket
+    .receive("error", resp => { console.log("Unable to join", resp) })
+
+  channel.on(`comments:${topicId}:new`, renderComment)
+
+  document.querySelector('button').addEventListener('click', () => {
+    const content = document.querySelector('textarea').value;
+
+    channel.push('comment:add', {content: content});
+  });
+}
+
+function renderComments(comments) {
+  const renderComments = comments.map(comment => {
+    return commentTemplate(comment)
+  })
+  document.querySelector('.collection').innerHTML = renderComments.join('')
+}
+
+function renderComment(event) {
+  const renderedComment = commentTemplate(event.comment)
+  document.querySelector('.collection').innerHTML += renderedComment;
+}
+
+function commentTemplate(comment) {
+  let email = 'Anonymous'
+  if (comment.user) {
+    email = comment.user.email
+  }
+  return `
+  <li class="collection-item">
+    ${comment.content}
+    <div class="secondary-content">
+      ${email}
+    </div>
+  </li>
+  `
+}
+// Now that you are connected, you can join channels with a topic:
+
+window.createSocket = createSocket;
